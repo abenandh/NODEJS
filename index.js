737c1930b631d91fdd39c5dc856f579bc21ff596
abenandh <abenandhvarmman@gmail.com>
2023-04-02 22:07:04 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const data = [
+  { id: 1, name: 'John Doe' },
+  { id: 2, name: 'Jane Doe' },
+];
+
+app.get('/api/data', (req, res) => {
+  res.send(data);
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
